7c6a3ea9adb9c8e15e882a5e77bc872718661b9f
Amit Patange <amitpatange88@gmail.com>
2018-09-13 20:54:51 +0530

    redis demo

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            string clientId = db.StringGet("ClientId");
+            string clientSecrete = db.StringGet("ClientScrete");
+            Console.WriteLine(string.Format("ClientSecrete = {0}     ClientId = {1}", clientSecrete, clientId));
+            Console.ReadLine();
+        }
+    }
+}
